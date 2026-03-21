0b03fda100f5e2cae285a316cf9ff9e2aa25c5a5
astrik476-debug <astrik476@gmail.com>
2026-03-21 21:32:19 +0530

    Update server.py

--- server.py
+++ server.py
@@ -31,7 +31,11 @@ def get_db():
     conn = psycopg2.connect(
         DATABASE_URL,
         sslmode='require',
-        connect_timeout=10
+        connect_timeout=30,
+        keepalives=1,
+        keepalives_idle=30,
+        keepalives_interval=10,
+        keepalives_count=5
     )
     return conn
 
